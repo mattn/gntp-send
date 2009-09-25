914d9fb2660cc6b41efcdbf5f8e546206fd0c8d9
The Missing <link@unlinked.vm.bytemark.co.uk>
2009-09-25 17:03:16 +0100

    Finish up udp support

--- headers/md5.h
+++ headers/md5.h
@@ -1,38 +1,22 @@
 #ifndef _MD5_H_
 #define _MD5_H_
 
-#ifndef uint8
-#define uint8 unsigned char
+
+#ifdef _MSC_VER
+#include <msinttypes/stdint.h>
+#else
+#include <stdint.h>
 #endif
- 
-#ifndef uint32
-#define uint32 unsigned long int
-#endif
- 
-#ifndef uint64
-# ifdef _WIN32
-# ifdef __GNUC__
-# define uint64 unsigned __int64
-# else
-# define uint64 unsigned _int64
-# endif
-# else
-# define uint64 unsigned long long
-# endif
-#endif
- 
-#ifndef byte
-# define byte unsigned char
-#endif
- 
+
+
 typedef struct {
-	uint32 total[2];
-	uint32 state[4];
-	uint8 buffer[64];
+	uint32_t total[2];
+	uint32_t state[4];
+	uint8_t buffer[64];
 } md5_context;
  
 void md5_starts(md5_context *ctx);
-void md5_update(md5_context *ctx, const uint8 *input, uint32 length);
-void md5_finish(md5_context *ctx, uint8 digest[16]);
+void md5_update(md5_context *ctx, const uint8_t *input, uint32_t length);
+void md5_finish(md5_context *ctx, uint8_t digest[16]);
 
 #endif /* _MD5_H_ */
--- source/growl.c
+++ source/growl.c
@@ -3,15 +3,17 @@
 #define EXPORT __declspec(dllexport)
 #else
 #define EXPORT
+#include <arpa/inet.h>
+#include <stdint.h>
 #endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <arpa/inet.h>
 
 #include "md5.h"
 #include "tcp.h"
+
 
 static const char hex_table[] = "0123456789ABCDEF";
 static char* string_to_hex_alloc(const char* str, int len) {
@@ -40,13 +42,13 @@ char* gen_password_hash_alloc(const char* password, const char* salt) {
 
 	memset(md5tmp, 0, sizeof(md5tmp));
 	md5_starts(&md5ctx);
-	md5_update(&md5ctx, (uint8*)password, strlen(password));
-	md5_update(&md5ctx, (uint8*)salt, strlen(salt));
-	md5_finish(&md5ctx, (uint8*)md5tmp);
+	md5_update(&md5ctx, (uint8_t*)password, strlen(password));
+	md5_update(&md5ctx, (uint8_t*)salt, strlen(salt));
+	md5_finish(&md5ctx, (uint8_t*)md5tmp);
 
 	md5_starts(&md5ctx);
-	md5_update(&md5ctx, (uint8*)md5tmp, 16);
-	md5_finish(&md5ctx, (uint8*)md5tmp);
+	md5_update(&md5ctx, (uint8_t*)md5tmp, 16);
+	md5_finish(&md5ctx, (uint8_t*)md5tmp);
 	md5digest = string_to_hex_alloc(md5tmp, 16);
 
 	return md5digest;
@@ -60,7 +62,7 @@ int growl( const char *const server,const char *const appname,const char *const 
 	char* salt;
         char* salthash;
         char* keyhash;
-        char* authheader = NULL;
+		char* authheader = NULL;
 
 	if (password) {
 		srand(time(NULL));
@@ -131,34 +133,134 @@ leave:
 	return (sock == 0) ? 0 : -1;
 }
 
+void growl_append_md5( unsigned char *const data , const int data_length , const char *const password )
+{
+	md5_context md5ctx;
+	char md5tmp[20];
+
+	memset(md5tmp, 0, sizeof(md5tmp));
+	md5_starts(&md5ctx);
+	md5_update(&md5ctx, (uint8_t*)data, data_length );
+	md5_update(&md5ctx, (uint8_t*)password, strlen(password));
+	md5_finish(&md5ctx, (uint8_t*)md5tmp);
+
+	memcpy( data + data_length , md5tmp , 16 );
+}
+
+int growl_udp_register( const char *const server , const char *const appname , const char *const notify , const char *const password  )
+{
+	int register_header_length = 26+strlen(appname)+strlen(notify);
+	unsigned char *data = malloc(register_header_length);
+	int pointer = 0;
+	int rc = 0;
+
+	uint8_t GROWL_PROTOCOL_VERSION  = 1;
+	uint8_t GROWL_TYPE_REGISTRATION = 0;
+
+	uint16_t appname_length = ntohs(strlen(appname));
+	uint16_t notify_length = ntohs(strlen(notify));
+	uint8_t notifications_count = 1;
+	uint8_t default_notifications_count = 1;
+	
+	memset( data , 0 ,  register_header_length );
+
+
+	pointer = 0;
+	memcpy( data + pointer , &GROWL_PROTOCOL_VERSION , 1 );	
+	pointer++;
+	memcpy( data + pointer , &GROWL_TYPE_REGISTRATION , 1 );
+	pointer++;
+	memcpy( data + pointer , &appname_length , 2 );	
+	pointer += 2;
+	memcpy( data + pointer , &notifications_count , 1 );	
+	pointer++;
+	memcpy( data + pointer, &default_notifications_count , 1 );	
+	pointer++;
+	sprintf( data + pointer , "%s" , appname );
+	pointer += strlen(appname);
+
+	memcpy( data + pointer, &notify_length , 2 );		
+	pointer +=2;
+	sprintf( data + pointer , "%s" , notify );
+	pointer += strlen(notify);
+	 
+	memcpy( data , &notifications_count , 1 );
+	pointer++;
+
+
+	growl_append_md5( data , pointer , password );
+	pointer += 16;
+
+	rc = growl_tcp_datagram( server , data , pointer );
+	free(data);
+	return rc;
+}
+
+
+int growl_udp_notify( const char *const server,const char *const appname,const char *const notify,const char *const title, const char *const message ,
+                                const char *const password )
+{
+	int notify_header_length = 28 + strlen(appname)+strlen(notify)+strlen(message)+strlen(title);
+	unsigned char *data = malloc(notify_header_length);
+	int pointer = 0;
+	int rc = 0;
+
+	uint8_t GROWL_PROTOCOL_VERSION  = 1;
+	uint8_t GROWL_TYPE_NOTIFICATION = 1;
+
+	uint16_t flags = ntohs(0);
+	uint16_t appname_length = ntohs(strlen(appname));
+	uint16_t notify_length = ntohs(strlen(notify));
+	uint16_t title_length = ntohs(strlen(title));
+	uint16_t message_length = ntohs(strlen(message));
+
+
+	memset( data , 0 ,  notify_header_length );
+	
+	pointer = 0;
+	memcpy( data + pointer , &GROWL_PROTOCOL_VERSION , 1 );	
+	pointer++;
+	memcpy( data + pointer , &GROWL_TYPE_NOTIFICATION , 1 );
+	pointer++;
+	memcpy( data + pointer , &flags , 2 );
+	pointer += 2;
+	memcpy( data + pointer , &notify_length , 2 );	
+	pointer += 2;
+	memcpy( data + pointer , &title_length , 2 );	
+	pointer += 2;
+	memcpy( data + pointer , &message_length , 2 );	
+	pointer += 2;
+	memcpy( data + pointer , &appname_length , 2 );	
+	pointer += 2;
+	sprintf( data + pointer , notify );
+	pointer += strlen(notify);
+	sprintf( data + pointer , title );
+	pointer += strlen(title);
+	sprintf( data + pointer , message );
+	pointer += strlen(message);
+	sprintf( data + pointer , appname );
+	pointer += strlen(appname);
+
+
+	growl_append_md5( data , pointer , password );
+	pointer += 16;
+
+
+	rc = growl_tcp_datagram( server , data , pointer );
+	free(data);
+	return rc;
+}
+
 int growl_udp( const char *const server,const char *const appname,const char *const notify,const char *const title, const char *const message ,
                                 const char *const icon , const char *const password , const char *url )
 {
-	char data[1024];
-	memset( data , 0 , 1024 );
-
-	data[0] = htons(1);			// protocol version 1 (md5);
-	data[1] = htons(0);			// type 0 (registration)
-	data[2] = htons(0);			// length of app name
-	data[3] = htons(strlen(appname));
-	data[4] = htons(1);			// number of notifications
-	data[5] = htons(1);			// number of notifications enabled by default
-	sprintf( data + 6 , "%s" , appname );	// application name
-
-	
-	int pointer = 6 + strlen(appname);
-
-
-	data[pointer] = htons(0);		// notification name length;
-	pointer++;
-	data[pointer] = htons(strlen(notify));
-	pointer++;
-	sprintf( data + pointer , "%s" , notify );
-
-	data[pointer] = htons(1);
-
-	
-	return growl_tcp_datagram( server , data , pointer );
+	int rc = growl_udp_register(  server ,  appname ,  notify , password  );
+	printf( "rc = %d\n" , rc );
+	if( rc == 0 )
+	{
+		rc = growl_udp_notify( server, appname, notify, title,  message , password );
+	}
+	return rc;
 }
 
 
