471336a492de3c1020989a5532920df087dd30ac
The Missing <link@redbrick.dcu.ie>
2009-09-29 21:08:36 +0100

    add udp growl to dll export

--- source/growl.c
+++ source/growl.c
@@ -251,6 +251,7 @@ int growl_udp_notify( const char *const server,const char *const appname,const c
 	return rc;
 }
 
+EXPORT
 int growl_udp( const char *const server,const char *const appname,const char *const notify,const char *const title, const char *const message ,
                                 const char *const icon , const char *const password , const char *url )
 {
