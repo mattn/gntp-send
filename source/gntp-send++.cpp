1b8a69df2315a9db73a25a1cac8c74a11f4f880c
unknown <Peter  Sinnott@.(none)>
2010-05-05 00:41:46 +0100

    Provide a slightly better example c++ application

--- source/gntp-send++.cpp
+++ source/gntp-send++.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
 	const char *n[2] = { "alice" , "bob" };
-	Growl *growl = new Growl(GROWL_UDP,"password","gntp_send++",(const char **const)n,2);
+	Growl *growl = new Growl(GROWL_TCP,NULL,"gntp_send++",(const char **const)n,2);
 	growl->Notify("bob","title","message");
 
 	delete(growl);
